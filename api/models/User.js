cbff51639a548bda7cc3ca52f0256e59adf20858
JCibeira <jacibcas@gmail.com>
2018-09-26 09:43:02 -0400

    config DB and model User

---
+++ api/models/User.js
@@ -0,0 +1,100 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    emailAddress: {
+      type: 'string',
+      required: true,
+      unique: true,
+      isEmail: true,
+      maxLength: 200
+    },
+
+    password: {
+      type: 'string',
+      required: true,
+      protect: true
+    },
+
+    name: {
+      type: 'string',
+      required: true,
+      maxLength: 120
+    },
+
+    lastName: {
+      type: 'string',
+      required: true,
+      maxLength: 120
+    },
+
+    phone: {
+      type: 'string',
+      required: true,
+      maxLength: 120
+    },
+
+    profile_picture: {
+      type: 'string',
+      maxLength: 120
+    },
+
+    passwordResetToken: {
+      type: 'string',
+    },
+
+    passwordResetTokenTime: {
+      type: 'string',
+      columnType: 'datetime'
+    },
+
+    emailProofToken: {
+      type: 'string',
+    },
+
+    emailProofTokenTime: {
+      type: 'string',
+      columnType: 'datetime'
+    },
+
+    emailStatus: {
+      type: 'boolean',
+      defaultsTo: false,
+    },
+
+    tosAcceptedByIp: {
+      type: 'string',
+      description: 'The IP (ipv4) address of the request that accepted the terms of service.',
+      extendedDescription: 'Useful for certain types of businesses and regulatory requirements (KYC, etc.)',
+      moreInfoUrl: 'https://en.wikipedia.org/wiki/Know_your_customer'
+    },
+
+    lastSeenAt: {
+      type: 'string',
+      columnType: 'datetime'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
